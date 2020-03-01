3ca42e9f1adeef7a2552c47ee3feaa68d9419599
TheScarastic <warabhishek@gmail.com>
2020-03-01 13:07:07 +0000

    toco: overlay: Import brightness overlays from MIUI
    Change-Id: Ib00b3817eece7bb8ff0acafb3d545e4324d7c26d

--- device.mk
+++ device.mk
@@ -10,6 +10,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 # API level, the device has been commercially launched on
 PRODUCT_SHIPPING_API_LEVEL := 29
 
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
