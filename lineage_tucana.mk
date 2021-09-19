b5da7dbf5da498a1bbaa7cf5d57bf8aaca213668
alibei <sebastiankotzias83@gmail.com>
2021-09-19 10:48:42 +0200

    tucana: LineageOS -> CipherOS

--- lineage_tucana.mk
+++ lineage_tucana.mk
@@ -14,6 +14,11 @@ $(call inherit-product, device/xiaomi/tucana/device.mk)
 # Inherit some common Cipher stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_FACE_UNLOCK_SUPPORTED := true
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.cipher.maintainer=alibei
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_tucana
 PRODUCT_DEVICE := tucana
