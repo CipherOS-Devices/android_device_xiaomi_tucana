a8dd0f387c44321f0e2c0bfc4950eba3af3e835b
alibei <sebastiankotzias83@gmail.com>
2021-09-21 19:23:33 +0200

    tucana: Use init to bypass SafetyNet

--- device.mk
+++ device.mk
@@ -40,6 +40,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qcom
 
+# Init
+TARGET_INIT_VENDOR_LIB := //$(LOCAL_PATH):libinit_tucana
+
 # IR
 PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
